6512a0687bcba96a414292eb32e8918368d6f178
rosecb <roseeaceb@gmail.com>
2020-10-01 15:55:14 -1000

    lab 8 ec

---
+++ Lab8/ec.js
@@ -0,0 +1,8 @@
+let age_count = 1; // start the age counter
+let age = 19;
+let stoppingValue = Math.ceil(Math.sqrt(age));
+
+while(age_count < stoppingValue) {
+    console.log(count++);
+}
+console.log(`Rose is ${age_count} years old`);
