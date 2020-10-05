98ba753a3ad4d3c1fd5d40923faf4448b3fed639
rosecb <roseeaceb@gmail.com>
2020-10-04 20:41:21 -1000

    Order and Arrays

---
+++ OrdersandArrays/ex1.js
@@ -0,0 +1,9 @@
+var student = [
+    'class0' = 'ITM352',
+    'class1' = 'BUS311',
+    'class2' = 'ACC201',
+    'class3' = 'FIN100',
+    'class4' = 'BUS310',
+    'class5' = 'MOM202',
+]
+console.log(student[2]);
---
+++ OrdersandArrays/ex2.js
@@ -0,0 +1,10 @@
+var student = [
+    firstName = 'Dan',
+    lastName = 'Kazman',
+    major = 'MIS',
+    year = '3',
+    id = '123456789',
+    onProbation = 'true',
+]
+console.log(push.onProbation='false');
+
