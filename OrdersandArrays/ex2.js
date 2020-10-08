d6cc30e775d163d28184444fda51387e69d2ef87
rosecb <roseeaceb@gmail.com>
2020-10-08 11:57:25 -1000

    Lab 10 - 10/8/20

--- OrdersandArrays/ex2.js
+++ OrdersandArrays/ex2.js
@@ -1,9 +1,9 @@
-var student = [
-    firstName = 'Dan',
-    lastName = 'Kazman',
-    major = 'MIS',
-    year = '3',
-    id = '123456789',
-    onProbation = 'true',
-]
-console.log(JSON.stringify(major));
+var student = {
+    firstName: 'Dan',
+    lastName: 'Kazman',
+    major: 'MIS',
+    year: '3',
+    id:'123456789',
+    onProbation: 'true',
+};
+console.log(student.major);
